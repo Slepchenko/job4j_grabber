f21ef3696e9c5e371fcc3e2f67f32a6805e32d11
Satrim <59724286+Satrim@users.noreply.github.com>
2022-01-29 08:04:24 +1200

    2. Парсинг HTML страницы. [#260358 #256859]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,9 +9,18 @@ public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".icon_cell");
-        for (Element td : row) {
-            Element parent = td.parent();
-            System.out.println(parent.child(5).text());
+        Elements row1 = doc.select(".postslisttopic");
+
+        for (int i = 0; i < row1.size(); i++) {
+
+            Element href = row1.get(i).child(0);
+
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+
+            Element parent = row.get(i).child(5);
+
+            System.out.println(parent.text());
         }
     }
 }

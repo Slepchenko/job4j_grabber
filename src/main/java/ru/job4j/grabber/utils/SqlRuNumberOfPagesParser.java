c21632d7c9b900af463b8068ac7cdbd500fecd68
Satrim <59724286+Satrim@users.noreply.github.com>
2022-02-09 16:20:04 +1200

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #261414]

--- src/main/java/ru/job4j/grabber/utils/SqlRuNumberOfPagesParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuNumberOfPagesParser.java
@@ -6,12 +6,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.time.format.DateTimeFormatter;
 
 public class SqlRuNumberOfPagesParser {
-
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter
-            .ofPattern("yyyy-MM-dd HH:mm");
 
     public static void main(String[] args) throws IOException {
         String path = "https://www.sql.ru/forum/job-offers/";
@@ -19,13 +15,10 @@ public class SqlRuNumberOfPagesParser {
             String page = String.format(path + "%s", i);
             Document doc = Jsoup.connect(page).get();
             Elements row = doc.select(".postslisttopic");
-            SqlRuDateTimeParser dateTimeParser = new SqlRuDateTimeParser();
 
             for (Element td : row) {
-                String parent = td.parent().children().get(5).text();
-                System.out.println(page);
+                System.out.println(td.child(0).attr("href"));
                 System.out.println(td.text());
-                System.out.println(dateTimeParser.parse(parent).format(FORMATTER));
                 System.out.println();
             }
 

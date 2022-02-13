672e6fddf9a6ff42793600ebd608586246f93cfc
Satrim <59724286+Satrim@users.noreply.github.com>
2022-02-14 09:18:39 +1200

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/grabber/utils/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuParse.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SqlRuParse implements Parse{
+public class SqlRuParse implements Parse {
 
     private final List<Post> lists = new ArrayList<>();
 

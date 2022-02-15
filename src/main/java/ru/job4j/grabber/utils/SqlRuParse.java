4fb808d54be3e3401e4830fcfd8f79a27cb0b78f
Satrim <59724286+Satrim@users.noreply.github.com>
2022-02-15 22:22:43 +1200

    2.4. SqlRuParse [#285213 #264336]

--- src/main/java/ru/job4j/grabber/utils/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuParse.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class SqlRuParse implements Parse {
 
-    private final List<Post> lists = new ArrayList<>();
+    public final List<Post> lists = new ArrayList<>();
 
     private final SqlRuDateTimeParser dateTimeParser;
 
@@ -20,27 +20,45 @@ public class SqlRuParse implements Parse {
     }
 
     @Override
-    public List<Post> list(String link) throws IOException {
-        for (int i = 1; i < 6; i++) {
-            String page = String.format(link + "%s", i);
-            Document doc = Jsoup.connect(page).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element element : row) {
-                lists.add(detail(element.child(0).attr("href")));
+    public List<Post> list(String link) {
+        try {
+            for (int i = 1; i < 6; i++) {
+                String page = String.format(link + "%s", i);
+                Document doc = Jsoup.connect(page).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element element : row) {
+                    Post post = detail(element.child(0).attr("href"));
+                    if (isRequiredTitle(post.getTitle())) {
+                        lists.add(post);
+                    }
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return lists;
     }
 
     @Override
-    public Post detail(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
-        return new Post(
-                doc.select(".messageHeader").get(0).ownText(),
-                link,
-                doc.select(".msgBody").get(1).text(),
-                dateTimeParser.parse(doc.select(".msgFooter").get(0).textNodes().get(0).text()
-                        .replace(" [", "").substring(1))
-        );
+    public Post detail(String link) {
+        Post post = null;
+        try {
+            Document doc = Jsoup.connect(link).get();
+            post = new Post(
+                    doc.select(".messageHeader").get(0).ownText(),
+                    link,
+                    doc.select(".msgBody").get(1).text(),
+                    dateTimeParser.parse(doc.select(".msgFooter")
+                            .get(0).textNodes().get(0).text()
+                            .replace(" [", "").substring(1)));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
+    }
+
+    private boolean isRequiredTitle(String name) {
+        return name.toLowerCase().contains("java") && !name.toLowerCase().contains("javascript");
     }
 }
